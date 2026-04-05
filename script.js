06ad80289667e0156d47a253f32646e3b228cda3
haomechatronics <haomechatronics@gmail.com>
2026-04-05 09:24:41 +0700

    Update script.js

--- script.js
+++ script.js
@@ -37,9 +37,7 @@ faqItems.forEach((item) => {
 
   button?.addEventListener('click', () => {
     const isActive = item.classList.contains('active');
-
     faqItems.forEach((faq) => faq.classList.remove('active'));
-
     if (!isActive) {
       item.classList.add('active');
     }
